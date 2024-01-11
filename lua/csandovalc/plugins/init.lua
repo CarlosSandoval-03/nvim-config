5ecf0f4b1eb2f757e23781bb9b9bf8346180efce
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-10 23:28:17 -0500

    feat(lazy): new plugins and set dependencies
    Add new plugins:
        - undotree
        - vim-fugitive
        - nvim-treesitter-context
        - zen-mode and twilight
        - copilot
        - cellular-atomaton (optional)
        - cloack
        - harpoon (v2)
    
    Add cloack base configuration in `plugins/configs'
    
    Use the `dependencies` option in the lazy configuration to make it easier to
    check the chain of dependencies.
    
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/plugins/init.lua
+++ lua/csandovalc/plugins/init.lua
@@ -1,13 +1,56 @@
 return {
-    "folke/tokyonight.nvim",
-    "nvim-lua/plenary.nvim",        -- Telescope Dependency
-    "nvim-tree/nvim-web-devicons",  -- Trouble Dependency
     {
-        "folke/trouble.nvim",
-         dependencies = { "nvim-tree/nvim-web-devicons" },
+		"nvim-lua/plenary.nvim",
+        name = "plenary",
     },
     {
-        "nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+        name = "web-devicons",
+    },
+    {
+		"folke/trouble.nvim",
+         dependencies = { "web-devicons" },
+    },
+    {
+		"nvim-treesitter/nvim-treesitter",
+        dependencies = { "plenary" },
         build = ":TSUpdate",
     },
+    {
+		"mbbill/undotree",
+    },
+    {
+		"tpope/vim-fugitive",
+    },
+    {
+		"nvim-treesitter/nvim-treesitter-context",
+    },
+    {
+		"folke/zen-mode.nvim",
+    },
+    {
+        "folke/twilight.nvim",
+    },
+    {
+		"github/copilot.vim",
+    },
+    {
+        -- This plugin is only for joke, can be removed
+		"eandrju/cellular-automaton.nvim",
+    },
+    {
+		"laytan/cloak.nvim",
+        opts = function()
+            return require "csandovalc.plugins.configs.cloak"
+        end,
+        config = function(_, opts)
+            require("cloak").setup(opts)
+        end,
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    }
 }
+
