588dfac85a7a6b19d9cbcfa545d770e5476d03ce
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-10 23:24:59 -0500

    feat(theme): new colorscheme and set priority
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/plugins/colorscheme.lua
+++ lua/csandovalc/plugins/colorscheme.lua
@@ -1,6 +1,12 @@
 return {
+    -- TODO: Script to change theme faster
+    {
+        "folke/tokyonight.nvim",
+    },
     {
         'rose-pine/neovim',
+        lazy = false,
+        priority = 1000,
         name = 'rose-pine',
         config = function()
             vim.cmd("colorscheme rose-pine")
