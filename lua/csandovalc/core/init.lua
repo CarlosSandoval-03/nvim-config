b9c6c4eba74def9d660772a987e0840da98cc609
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-15 00:36:49 -0500

    feat(autocmd): add remove white spaces and yank copied text
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/core/init.lua
+++ lua/csandovalc/core/init.lua
@@ -2,3 +2,29 @@ require("csandovalc.core.configs.set")
 require("csandovalc.core.configs.remap")
 require("csandovalc.core.configs.lazy_init")
 
+local augroup = vim.api.nvim_create_augroup
+local csandovalGroup = augroup('csandovalc', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = csandovalGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
