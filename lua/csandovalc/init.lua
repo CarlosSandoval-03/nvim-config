998b1ac5898c1cd730149c1a3dae1235c4770ae6
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-14 19:56:38 -0500

    feat(theme): add colorscheme setup and config
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/init.lua
+++ lua/csandovalc/init.lua
@@ -1,2 +1,14 @@
 require("csandovalc.core")
 require("lazy").setup("csandovalc.plugins")
+
+-- Load the colorscheme
+function SetTheme(name)
+    theme = name or "tokyonight"
+    vim.cmd.colorscheme(theme)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+SetTheme()
+
