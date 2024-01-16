9a2d97dd0d81a86cb78113d5a9fe3df140edfdb3
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-15 19:52:52 -0500

    feat(trouble): adjust keybindings
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/plugins/configs/trouble.lua
+++ lua/csandovalc/plugins/configs/trouble.lua
@@ -1,11 +1,11 @@
-vim.keymap.set("n", "<leader>tt", function()
+vim.keymap.set("n", "<leader>d", function()
     require("trouble").toggle()
 end)
 
-vim.keymap.set("n", "<leader>tn", function()
+vim.keymap.set("n", "[d", function()
     require("trouble").next({ skip_groups = true, jump = true })
 end)
 
-vim.keymap.set("n", "<leader>tp", function()
+vim.keymap.set("n", "]d", function()
     require("trouble").previous({ skip_groups = true, jump = true })
 end)
