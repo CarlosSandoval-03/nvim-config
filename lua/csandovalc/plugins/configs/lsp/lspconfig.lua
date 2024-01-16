f8d4171971b063a9c2386a92950ce4b967a64b3b
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-15 20:19:23 -0500

    feat(lsp): add attach
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/plugins/configs/lsp/lspconfig.lua
+++ lua/csandovalc/plugins/configs/lsp/lspconfig.lua
@@ -36,7 +36,6 @@ lspconfig.cmake.setup({})
 
 -- Rust
 lspconfig.rust_analyzer.setup({})
-
 -- SystemVerilog
 lspconfig.svlangserver.setup({})
 
@@ -110,3 +109,16 @@ lspconfig.lua_ls.setup({
 -- TOML
 lspconfig.taplo.setup({})
 
+-------------------------------------------------------------------------------
+--- General Settings
+-------------------------------------------------------------------------------
+local function attach(opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.document_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end
