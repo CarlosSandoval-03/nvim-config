1ec733e6e7916372e45a9e7deae8ea15a782f468
Carlos Sandoval <csandovalc@unal.edu.co>
2024-01-17 20:24:40 -0500

    chore: remove whitespace
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/plugins/colorscheme.lua
+++ lua/csandovalc/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         config = function()
             require("tokyonight").setup({
-                style = "night",  
+                style = "night",
                 transparent = true,
                 terminal_colors = true,
                 styles = {
