de2433d38557537ccc3e25ce16e8bdd6d41d9700
Carlos Sandoval <csandovalc@unal.edu.co>
2024-11-02 00:55:33 -0500

    feat(plugins): add go plugin and update plugins
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- lua/csandovalc/plugins/init.lua
+++ lua/csandovalc/plugins/init.lua
@@ -123,5 +123,18 @@ return {
       require "csandovalc.plugins.configs.arduino"
     end,
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      --"neovim/nvim-lspconfig",
+      --"nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 }
-
